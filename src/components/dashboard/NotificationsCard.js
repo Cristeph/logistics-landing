c7fefe604ec8836a8eed58a8946cb03c9cc6cb29
lasbrey <lazarusosilawal.com>
2024-09-23 21:44:13 +0100

    initialize development branch and add components to dashboard

---
+++ src/components/dashboard/NotificationsCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { FaPhone, FaComment } from "react-icons/fa";
+
+const NotificationsCard = () => {
+  return (
+    <div className="my-5 px-5 py-2 bg-white rounded-lg shadow-md max-w-md mx-auto">
+      <div className="flex justify-between items-center mb-4">
+        <div>
+          <div className="flex items-center">
+            <p className="font-bold text-xl mr-2">Notifications</p>
+          </div>
+        </div>
+      </div>
+      
+      <div className="flex items-center mb-4">
+        <div className="w-full bg-gray-200 h-1">
+          <div className="bg-blue-500 h-1 w-1/2"></div>
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <div className="flex items-center mb-2">
+          <div className="bg-blue-100 p-2 rounded-full mr-3">
+            <div className="bg-blue-500 w-3 h-3 rounded-full"></div>
+          </div>
+          <div className="flex-grow">
+            <p className="font-semibold">Warsaw, Poland</p>
+            <p className="text-sm text-gray-500">Branch 39</p>
+          </div>
+          <div className="text-right text-sm text-gray-500">
+            <p>06.12.2023</p>
+            <p>09:18</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationsCard;
--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import Stats from 'components/Stats';
+import React from "react";
+import Stats from "components/Stats";
+import Card from "components/dashboard/Card";
+import TrackingOrders from "components/dashboard/TrackingOrders";
+import NotificationsCard from "components/dashboard/NotificationsCard";
 
 const Overview = () => {
-    return (
-        <>
-            <Stats />
-        </>
-    )
-}
+  return (
+    <div className="m-4">
+      {/* <Stats /> */}
+      {/* Cards */}
+      <Card />
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        <div className="lg:col-span-3 md:col-span-2">
+          <TrackingOrders />
+        </div>
+        <div className="lg:col-span-2 md:col-span-1">
+          <NotificationsCard />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Overview
+export default Overview;

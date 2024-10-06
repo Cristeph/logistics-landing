96820c4fe2402c4ff0ccfb3c1f2a5884440c4c52
Andrewbizz <ezeaniandrew8197@gmail.com>
2024-10-06 12:26:23 +0100

    my own crimes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
                         <a href="/#" className="hover:text-red-400">
                             Home
                         </a>
-                    </li>
+                    </li> 
                     <li>
                         <a href="/#" className="hover:text-red-400">
                             Services

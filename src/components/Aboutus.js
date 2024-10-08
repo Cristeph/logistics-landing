7097fd1319e2777407245d2883a90b5067d489ca
Andrewbizz <ezeaniandrew8197@gmail.com>
2024-10-08 01:42:07 +0100

    modified

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { IoCopy } from "react-icons/io5";
+  
 
 const Aboutus = ({ counters }) => {
   const elementsRef = useRef([]); // Store references to multiple elements
